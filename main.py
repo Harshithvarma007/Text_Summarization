771a172e65fe3f6272088893a8377a76f4ddbe15
Harshithvarma007 <112501476+Harshithvarma007@users.noreply.github.com>
2024-04-16 04:37:07 +0530

    added Gitignore

--- main.py
+++ main.py
@@ -1,3 +1,14 @@
+from Text_Summarizer.pipeline.stage_01_data_ingestion import DataIngestionTrainingPipeline
+
 from Text_Summarizer.logging import logger
 
-logger.info("welcome to the log")
+
+STAGE_NAME = "Data Ingestion Stage"
+try:
+    logger.info(f">>>>> stage {STAGE_NAME} started <<<<<<<<")
+    dataingestion=DataIngestionTrainingPipeline()
+    dataingestion.main()
+    logger.info(f">>>>>>>stage {STAGE_NAME} completed <><<<<<<<<")
+except Exception as e:
+    logger.exception(e)
+    raise e
