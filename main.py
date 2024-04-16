184ea2438301a6d7088ba15653b708cc0d6f3226
Harshithvarma007 <112501476+Harshithvarma007@users.noreply.github.com>
2024-04-16 05:37:57 +0530

    Data Transformation.py

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 from Text_Summarizer.pipeline.stage_01_data_ingestion import DataIngestionTrainingPipeline
 from Text_Summarizer.pipeline.stage_02_data_validation import DataValidationPipeline
+from Text_Summarizer.pipeline.stasge_03_data_transformation import DataTransformationPipeline
 from Text_Summarizer.logging import logger
 
 
@@ -18,6 +19,16 @@ STAGE_NAME = "Data Validation Stage"
 try:
     logger.info(f">>>>> stage {STAGE_NAME} started <<<<<<<<")
     datavalidation=DataValidationPipeline()
+    datavalidation.main()
+    logger.info(f">>>>>>>stage {STAGE_NAME} completed <><<<<<<<<")
+except Exception as e:
+    logger.exception(e)
+    raise e
+
+STAGE_NAME = "Data Transformation Stage"
+try:
+    logger.info(f">>>>> stage {STAGE_NAME} started <<<<<<<<")
+    datavalidation=DataTransformationPipeline()
     datavalidation.main()
     logger.info(f">>>>>>>stage {STAGE_NAME} completed <><<<<<<<<")
 except Exception as e:
---
+++ src/Text_Summarizer/pipeline/stasge_03_data_transformation.py
@@ -0,0 +1,11 @@
+from Text_Summarizer.config.configuration import ConfigurationManager
+from Text_Summarizer.components.data_trandformation import DataTransformation
+from Text_Summarizer.logging import logger
+class DataTransformationPipeline:
+    def __init__(self) -> None:
+        pass
+    def main(self):
+        config = ConfigurationManager()
+        data_transformation_config = config.get_data_transformation_config()
+        data_transformation = DataTransformation(config=data_transformation_config)
+        data_transformation.convert()        
